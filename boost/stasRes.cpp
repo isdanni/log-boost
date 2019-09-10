a98da2adcbd45a20ac2d7400ca6a235ed42ad014
Danni <38550500+isdanni@users.noreply.github.com>
2019-09-10 14:43:57 +0800

    Update stasRes.cpp

--- boost/stasRes.cpp
+++ boost/stasRes.cpp
@@ -1,11 +1,11 @@
 #include<Windows.h>
 
-void  CALLBACK HideWnd(HWND   hwnd,UINT   uMsg,UINT   idEvent,DWORD   dwTime);//回调函数声明
+void  CALLBACK HideWnd(HWND   hwnd,UINT   uMsg,UINT   idEvent,DWORD   dwTime);
 
 ShowWindow(true);
 SetTimer(m_hWnd,1, 1000, HideWnd);
 
-void  CALLBACK HideWnd(HWND   hwnd,UINT   uMsg,UINT   idEvent,DWORD   dwTime)//回调函数
+void  CALLBACK HideWnd(HWND   hwnd,UINT   uMsg,UINT   idEvent,DWORD   dwTime)
 {
 	ShowWindow(hwnd, false);
 	KillTimer(hwnd,1);
